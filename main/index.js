37d950d909d49270a2432dee3df33478439c8293
camilo rios <camilototus180@gmail.com>
2023-07-21 09:42:19 -0500

    se configuro la seccion de favoritos y se llamo la API de favourites

--- main/index.js
+++ main/index.js
@@ -1,8 +1,11 @@
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_mb8YiaKtFcQuQDAa2eYb8bWJ8mfZbV5XtGuxJ2A693pqvTpHzpfBmOv7KuyStRd9";
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2";
 
+const API_URL_FAVOURITES = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_mb8YiaKtFcQuQDAa2eYb8bWJ8mfZbV5XtGuxJ2A693pqvTpHzpfBmOv7KuyStRd9"
+
+const spanError = document.getElementById("error")
 const btn = document.querySelector(".load");
 
-btn.addEventListener("click", reload);
+btn.addEventListener("click", reloadRandom);
 
 // function reload() {
 //     fetch(URL)
@@ -13,17 +16,35 @@ btn.addEventListener("click", reload);
 //         })
 
 // }
-async function reload() {
-  const res = await fetch(API_URL);
+async function reloadRandom() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
+  console.log("random")
   console.log(data);
 
-  const img1 = document.getElementById("img1");
-  const img2 = document.getElementById("img2");
-  const img3 = document.getElementById("img3");
-  img1.src = data[0].url
-  img2.src = data[1].url
-  img3.src = data[2].url
+  if(res.status !== 200) {
+    spanError.innerHTML = "hubo en Error " + spanError + data.message;
+  } else {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    img1.src = data[0].url
+    img2.src = data[1].url
+  }
+
 }
 
-reload();
+async function reloadFavourites() {
+  const res = await fetch(API_URL_FAVOURITES);
+  const data = await res.json();
+  console.log("favoritos")
+  console.log(data);
+
+  if(res.status !== 200) {
+    spanError.innerHTML = "hubo en Error " + spanError
+  }
+}
+
+
+
+reloadRandom();
+reloadFavourites();
